c9523f9eb9e57cbc93f04d02828c724e97382a4a
Huber <huber.z@shopperplus.com>
2022-03-29 23:18:47 +0800

    1

--- linked_list/list.rb
+++ linked_list/list.rb
@@ -74,6 +74,40 @@ class LinkedList
     p "no: #{temp.no}, #{temp.name}, #{temp.nickname}, next: #{temp.next&.no}" while temp = temp.next
   end
 
+  def get_effective_size
+    i = 0
+    temp = head
+    i += 1 while temp = temp.next
+    i
+  end
+
+  def get_countdown_node(index)
+    step = get_effective_size - index
+    return '节点不存在' if step < 0
+    temp = head.next
+    step.times { temp = temp.next }
+    p temp.to_s
+    temp    
+  end
+
+  def revert_list
+    return true if !head.next || !head.next.next
+
+    temp_head = ListNode.new
+    cur = head.next
+    _next = nil
+
+    while cur
+      _next = cur.next
+      cur.next = temp_head.next
+      temp_head.next = cur
+      cur = _next
+    end
+
+    head.next = temp_head.next
+    temp_head.next = nil
+  end
+
   private
   
 end
@@ -89,26 +123,12 @@ class ListNode
   end
 
   def to_s
-    "节点信息 #{@no}, #{@name}, #{@nickname}"
+    "节点信息 #{@no}, #{@name}, #{@nickname} #{self.next&.no}"
   end
 
 end
 
-list = LinkedList.new
-list.add_by_order ListNode.new 5, 'a', 'aa'
-list.add_by_order ListNode.new 4, 'd', 'dd'
-list.add_by_order ListNode.new 4, 'd', 'dd'
-list.add_by_order ListNode.new 2, 'b', 'bb'
-list.add_by_order ListNode.new 9, 'c', 'cc'
-list.add_by_order ListNode.new 9, 'c', 'cc'
-list.list
-p "--------1"
-list.update(ListNode.new(2, 'ba', 'bbaa'))
-list.list
-p "--------2"
-list.destroy(9)
-list.list
-p "--------3"
+p "按顺序添加"
 l = LinkedList.new
 l.add ListNode.new 5, 'a', 'aa'
 l.add ListNode.new 4, 'd', 'dd'
@@ -117,3 +137,31 @@ l.add ListNode.new 2, 'b', 'bb'
 l.add ListNode.new 9, 'c', 'cc'
 l.add ListNode.new 9, 'c', 'cc'
 l.list
+
+p "按排序添加"
+list = LinkedList.new
+list.add_by_order ListNode.new 1, 'a', 'aa'
+list.add_by_order ListNode.new 3, 'd', 'dd'
+list.add_by_order ListNode.new 2, 'd', 'dd'
+list.add_by_order ListNode.new 6, 'b', 'bb'
+list.add_by_order ListNode.new 5, 'c', 'cc'
+list.add_by_order ListNode.new 4, 'c', 'cc'
+list.list
+
+p "更新节点"
+list.update(ListNode.new(2, 'ba', 'bbaa'))
+list.list
+
+p "删除节点"
+list.destroy(6)
+list.list
+
+p "获取链表长度"
+p list.get_effective_size
+
+p "获取链表倒数的有效节点"
+p list.get_countdown_node(3)
+
+p "反序链表"
+list.revert_list
+list.list
